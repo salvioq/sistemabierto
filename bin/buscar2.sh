3717f2f0bd0a3e36480c05d9c97464d133af09be
Salvio Quiralte <8882468+salvioq@users.noreply.github.com>
2018-11-06 18:54:28 +0100

    buscar3.sh

--- bin/buscar2.sh
+++ bin/buscar2.sh
@@ -1,21 +1,26 @@
 #! /bin/sh
-#Descripcion: recibe un parametro $1
-# comprueba con if -z si tiene longitud 0
-# en cuyo caso se indica el uso correcto
-# si tiene longitud distinta de cero se lanza find
-# con redireccion al fichero temporal /tmp/busqueda
-# se cuentan las lineas con wc -l
+#Descripcion: recibe dos parametros $1 para lugar y $2 para nombre fichero
+#Comprueba con if test -z ... si cada parametro tiene longitud 0
+# En caso de que $2 o $1 este vacio se indica el uso correcto
+#Si ambos tienen longitud distinta de cero se lanza find
+# se redirecciona la salida al fichero temporal /tmp/busqueda
+# se redirecciona el error a /dev/null
+#Se cuentan las lineas de /tmp/busqueda con wc -l
 # y se muestran los resultados
 #
-echo Buscar2 utilizando $*
-if [ -z $1 ]; then
-	echo "Uso: $0 cadena ";
+echo "Buscar2 utilizando lugar $1 y nombre $2, resto parametros $*";
+if [ -z $2 ]; then
+	echo "Uso: $0 $1 cadena ";
 else 
-	echo BUSCANDO $1;
-	find / -name $1 >/tmp/busqueda 2>/dev/null;
-	CUANTOS=`cat /tmp/busqueda | wc -l`;
-	echo "Se han encontrado $CUANTOS coincidencias";
-	if [ $CUANTOS!="0" ]; then 
-		cat /tmp/busqueda; 
+	if [ -z $1 ]; then
+		echo "Uso: $0 ruta cadena ";
+	else 
+		echo BUSCANDO en  $1 ficheros con nombre $2;
+		find $1 -name $2 >/tmp/busqueda 2>/dev/null;
+		CUANTOS=`cat /tmp/busqueda | wc -l`;
+		echo "Se han encontrado $CUANTOS coincidencias";
+		if [ $CUANTOS!="0" ]; then 
+			cat /tmp/busqueda; 
+		fi
 	fi
 fi
